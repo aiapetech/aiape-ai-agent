b4564bc65941509a39cea8ae61c0efcf10b065ef
Bao Ho <79841883+quocbao-olli@users.noreply.github.com>
2025-03-20 14:32:45 +0700

    add cors for localhost

--- core/config.py
+++ core/config.py
@@ -47,7 +47,9 @@ class Settings(BaseSettings):
     @property
     def all_cors_origins(self) -> list[str]:
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
-            self.FRONTEND_HOST
+            self.FRONTEND_HOST,
+            "http://localhost:8080"
+
         ]
 
     PROJECT_NAME: str = os.getenv("PROJECT_NAME")
