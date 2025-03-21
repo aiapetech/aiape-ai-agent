3ecd8a8fa9d59ad6fe785516969ae8f837f06200
Bao Ho <79841883+quocbao-olli@users.noreply.github.com>
2025-03-21 14:07:45 +0700

    update schemas

--- schemas/liquidity_bot.py
+++ schemas/liquidity_bot.py
@@ -13,6 +13,9 @@ class Content(BaseModel):
     token_address:Union[str, None] = None
     token_img:Union[str, None] = None
     #token_data: Union[dict, None] = None
+    gmgn_url: Union[str, None] = None
+    dexscreener_url: Union[str, None] = None
+    gmgn_bot_url: Union[str, None] = None
 
 class Contents(BaseModel):
     result: List[Content]
